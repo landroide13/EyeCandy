0189b4bafab6a46b08a99702f26da13799522b72
landroide13 <lazaro02@hotmail.com>
2021-06-30 17:14:55 +1200

    Js second Slider

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,4 @@
 const slides = document.querySelectorAll(".slide");
-const slides2 = document.querySelectorAll(".slide2");
 const auto = true;
 const inter = 3000;
 
@@ -7,19 +6,43 @@ let slideInter;
 
 const nextSlide = () => {
   const current = document.querySelector(".current");
+  
   current.classList.remove("current");
+  
 
   if (current.nextElementSibling) {
     current.nextElementSibling.classList.add("current");
   } else {
     slides[0].classList.add("current");
-    slides2[0].classList.add("current");
   }
-  setTimeout(() => current.classList.remove("current"));
+  setTimeout(() => current.classList.remove("current")); 
 };
 
 if (auto) {
   slideInter = setInterval(nextSlide, inter);
 }
 
+//Second Slider
+
+const slides2 = document.querySelectorAll(".slide2");
+const on = true;
+const t = 3000;
+
+let frec;
+
+const moveSlide = () => {
+  const current2 = document.querySelector(".current2");
+  current2.classList.remove("current2");
+
+  if (current2.nextElementSibling) {
+    current2.nextElementSibling.classList.add("current2");
+  } else {
+    slides2[0].classList.add("current2");
+  }
+  setTimeout(() => current2.classList.remove("current2"));
+};
+
+if (on) {
+  frec = setInterval(moveSlide, t);
+}
 
